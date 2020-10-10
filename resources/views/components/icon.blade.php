8a0a92924f325067ab079e787b3e042ec2a21701
Mustafa Genç <mustafa.genc@enkasystems.com>
2020-10-10 22:35:19 +0300

    Icon component eklendi

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,14 @@
+<svg width="1em" height="1em" viewBox="0 0 16 16" {{ $attributes->merge(['class' => 'bi ' . $type]) }} fill="{{ $fill }}" xmlns="http://www.w3.org/2000/svg">
+    @if ($type == "bi-chevron-right")
+        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+    @endif
+    @if ($type == "bi-three-dots-vertical")
+        <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+    @endif
+    @if ($type == "bi-check")
+        <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+    @endif
+    @if ($type == "bi-x")
+        <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+    @endif
+</svg>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,10 +27,10 @@
                     Faydalı Bağlantılar
                 </h5>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><i class="fas fa-chevron-right fa-sm"></i> <a class="card-link" href="https://www.kiyiemniyeti.gov.tr/duyurular">Kıyı Emniyeti Genel Müdürlüğü Duyuru Listesi</a></li>
-                    <li class="list-group-item"><i class="fas fa-chevron-right fa-sm"></i> <a class="card-link" href="http://www.tacallbook.org/">TA Callback</a></li>
-                    <li class="list-group-item"><i class="fas fa-chevron-right fa-sm"></i> <a class="card-link" href="https://www.ta7om.com/">TA7OM - Amateur Radio</a></li>
-                    <li class="list-group-item"><i class="fas fa-chevron-right fa-sm"></i> <a class="card-link" href="http://www.ta1lsx.com/">TA1LSX Blog</a></li>
+                    <li class="list-group-item"><x-icon type="bi-chevron-right" fill="black" /> <a class="card-link" href="https://www.kiyiemniyeti.gov.tr/duyurular">Kıyı Emniyeti Genel Müdürlüğü Duyuru Listesi</a></li>
+                    <li class="list-group-item"><x-icon type="bi-chevron-right" fill="black" /> <a class="card-link" href="http://www.tacallbook.org/">TA Callback</a></li>
+                    <li class="list-group-item"><x-icon type="bi-chevron-right" fill="black" /> <a class="card-link" href="https://www.ta7om.com/">TA7OM - Amateur Radio</a></li>
+                    <li class="list-group-item"><x-icon type="bi-chevron-right" fill="black" /> <a class="card-link" href="http://www.ta1lsx.com/">TA1LSX Blog</a></li>
                 </ul>
             </div>
         </div>
